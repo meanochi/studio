ac403cb6c0000f4694b02c4991abb948b6f550f2
Lopiansky <michal.icecream@gmail.com>
2025-06-19 20:58:52 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/recipes/[id]/page.tsx
+++ src/app/recipes/[id]/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useState, useRef }_ from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { useParams, useRouter } from 'next/navigation';
 import Image from 'next/image';
 import Link from 'next/link';
