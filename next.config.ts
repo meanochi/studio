5ad4450174dfc34a1666df89950ea5d9c812eb3b
Lopiansky <michal.icecream@gmail.com>
2025-07-26 21:13:45 +0000

    offline mode isnt working

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,33 @@
 
 import type {NextConfig} from 'next';
 import withPWAInit from '@ducanh2912/next-pwa';
+import {
+  RuntimeCaching,
+} from '@ducanh2912/next-pwa';
+
+const runtimeCaching: RuntimeCaching[] = [
+    {
+      urlPattern: /^https?.*/,
+      handler: 'StaleWhileRevalidate',
+      options: {
+        cacheName: 'http-cache',
+        expiration: {
+          maxEntries: 200,
+          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
+        },
+        cacheableResponse: {
+          statuses: [0, 200],
+        },
+      },
+    },
+  ];
 
 const withPWA = withPWAInit({
   dest: 'public',
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === 'development',
+  runtimeCaching,
 });
 
 const nextConfig: NextConfig = {
