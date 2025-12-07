49d8ba8e8432551ec1c5242ec2e5fb2dc00ce2ae
δϽ'N <michal.icecream@gmail.com>
2025-12-07 02:10:45 +0000

    still

--- next.config.js
+++ next.config.js
@@ -1,27 +1,4 @@
 
-const withPWAInit = require('@ducanh2912/next-pwa').default;
-
-const withPWA = withPWAInit({
-  dest: 'public',
-  disable: true,
-  register: true,
-  cacheStartUrl: false, // Set to false to avoid auth proxy conflicts
-  dynamicStartUrl: true,
-  dynamicStartUrlRedirect: '/',
-  fallbacks: {
-    document: "/offline",
-  },
-  cacheOnFrontEndNav: true,
-  aggressiveFrontEndNavCaching: false,
-  reloadOnOnline: true,
-  workboxOptions: {
-    skipWaiting: true,
-    // Exclude all Google APIs from Workbox caching, letting Firestore's own persistence handle it.
-    exclude: [/^https?:\/\/firestore\.googleapis\.com\/.*/, /^https?:\/\/.*\.googleapis\.com\/.*/],
-  },
-   
-});
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
@@ -66,4 +43,4 @@ const nextConfig = {
   },
 };
 
-module.exports = withPWA(nextConfig);
+module.exports = nextConfig;
