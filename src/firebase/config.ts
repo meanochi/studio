9ed108c577438ed9751873c1278a65747e2374f5
δϽ'N <michal.icecream@gmail.com>
2025-12-07 02:56:50 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "family-cookbook-ajj0n",
+  "appId": "1:1001135814027:web:fefb9c175e5ff1e6d2ab8e",
+  "apiKey": "AIzaSyASF7deNn4S17yZLwUmMOU122LxjTI0ICU",
+  "authDomain": "family-cookbook-ajj0n.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1001135814027"
+};
